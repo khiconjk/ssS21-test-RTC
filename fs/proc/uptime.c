6ba481223862c7ec2ba70d48b18f5146a3c64e8d
khiconjk <khiconjk@gmail.com>
2026-04-28 21:11:56 +0700

    Update uptime.c

--- fs/proc/uptime.c
+++ fs/proc/uptime.c
@@ -7,29 +7,42 @@
 #include <linux/time.h>
 #include <linux/kernel_stat.h>
 #include <linux/timekeeping.h>
-#include <linux/time64.h> // Header quan trọng cho ns_to_timespec64
+#include <linux/time64.h>
+#include <linux/jiffies.h>
+
+extern uint64_t arch_sys_boot_offset;
 
 static int uptime_proc_show(struct seq_file *m, void *v)
 {
 	struct timespec64 uptime;
-	u64 nsec, idle_nsec;
-    u32 variator;
+	u64 total_uptime_nsec;
+	u64 idle_nsec;
+	u32 variator;
 
-	nsec = ktime_get_boot_fast_ns();
-    
-    /* Sửa tên hàm ở đây */
-	uptime = ns_to_timespec64(nsec); 
-	
-	/* Fake Idle time: 75% */
+	ktime_get_boottime_ts64(&uptime);
+
+	/* --- GHOST UPTIME OFFSET --- */
+	if (arch_sys_boot_offset > 0) {
+		// Cộng thêm số giây ảo vào uptime
+		uptime.tv_sec += (arch_sys_boot_offset / 1000000000ULL);
+	}
+	/* --------------------------- */
+
+	/* Tính tổng số nano giây uptime (ĐÃ BAO GỒM THỜI GIAN FAKE) */
+	total_uptime_nsec = ((u64)uptime.tv_sec * NSEC_PER_SEC) + uptime.tv_nsec;
+
+	/* Fake Idle time: Giao động 72% - 74% của tổng Uptime */
 	variator = (u32)(jiffies % 3); 
-    idle_nsec = (nsec * (72 + variator)) / 100; 
+	idle_nsec = (total_uptime_nsec * (72 + variator)) / 100; 
 
+	/* In ra file /proc/uptime theo chuẩn: <uptime_giây> <idle_giây> */
 	seq_printf(m, "%lu.%02lu %lu.%02lu\n",
-        (unsigned long) uptime.tv_sec,
-        (uptime.tv_nsec / (NSEC_PER_SEC / 100)),
-        (unsigned long) (idle_nsec / NSEC_PER_SEC),
-        ((idle_nsec % NSEC_PER_SEC) / (NSEC_PER_SEC / 100)));
-    return 0;
+		(unsigned long) uptime.tv_sec,
+		(uptime.tv_nsec / (NSEC_PER_SEC / 100)),
+		(unsigned long) (idle_nsec / NSEC_PER_SEC),
+		(unsigned long) ((idle_nsec % NSEC_PER_SEC) / (NSEC_PER_SEC / 100)));
+		
+	return 0;
 }
 
 static int __init proc_uptime_init(void)
