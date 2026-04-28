7fc2c7a9c1584d9f096b3e733501c25cadb288ce
khiconjk <khiconjk@gmail.com>
2026-04-28 23:41:20 +0700

    Update ghost_uptime.c

--- kernel/ghost_uptime.c
+++ kernel/ghost_uptime.c
@@ -10,9 +10,10 @@ void ghost_uptime_init(void)
 	u32 random_days;
 
 	get_random_bytes(&random_days, sizeof(random_days));
-	random_days = 16 + (random_days % 11);
+	random_days = 16 + (random_days % 6);
 
-	arch_sys_boot_offset = (u64)random_days * 86400ULL * 1000000000ULL;
+	arch_sys_boot_offset =
+		(u64)random_days * 86400ULL * 1000000000ULL;
 }
 
 static int __init ghost_uptime_module_init(void)
