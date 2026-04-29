132e0b03f02fecd345ddb784a8ef57e86b9eaa6e
khiconjk <khiconjk@gmail.com>
2026-04-29 15:28:40 +0700

    Implement VDSO disable for syscall enforcement
    Added comments for disabling VDSO to force syscall calls.

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -342,10 +342,11 @@ out:
 #endif /* !CONFIG_COMPAT_VDSO */
 
 int aarch32_setup_additional_pages(struct linux_binprm *bprm, int uses_interp)
+{
 /* --- GHOST UPTIME: TẮT VDSO ĐỂ ÉP APP GỌI SYSCALL --- */
 	return 0;
 /* ---------------------------------------------------- */
-{
+
 	struct mm_struct *mm = current->mm;
 	int ret;
 
@@ -406,10 +407,11 @@ arch_initcall(vdso_init);
 
 int arch_setup_additional_pages(struct linux_binprm *bprm,
 				int uses_interp)
+{
+	
 /* --- GHOST UPTIME: TẮT VDSO ĐỂ ÉP APP GỌI SYSCALL --- */
 	return 0;
 /* ---------------------------------------------------- */
-{
 	struct mm_struct *mm = current->mm;
 	int ret;
 
