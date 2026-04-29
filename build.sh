1f40ad88f24128e4441bb53ff7c14b47b0be4d92
khiconjk <khiconjk@gmail.com>
2026-04-29 10:38:24 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -48,32 +48,6 @@ cat > include/linux/ghost_uptime.h <<'EOF'
 #endif
 EOF
 
-cat > kernel/ghost_uptime.c <<'EOF'
-#include <linux/types.h>
-#include <linux/random.h>
-#include <linux/init.h>
-#include <linux/ghost_uptime.h>
-
-u64 arch_sys_boot_offset = 0;
-
-void ghost_uptime_init(void)
-{
-    u32 random_days;
-    get_random_bytes(&random_days, sizeof(random_days));
-    random_days = 15 + (random_days % 6);
-
-    arch_sys_boot_offset =
-        (u64)random_days * 86400ULL * 1000000000ULL;
-}
-
-static int __init ghost_uptime_module_init(void)
-{
-    ghost_uptime_init();
-    return 0;
-}
-early_initcall(ghost_uptime_module_init);
-EOF
-
 grep -q ghost_uptime.o kernel/Makefile || echo "obj-y += ghost_uptime.o" >> kernel/Makefile
 
 # ================= PATCH STAT =================
