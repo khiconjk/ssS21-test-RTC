8c1cbb61eb84e6c80855bceba9bbe80489f04de5
khiconjk <khiconjk@gmail.com>
2026-04-29 15:58:38 +0700

    Update uptime.c

--- fs/proc/uptime.c
+++ fs/proc/uptime.c
@@ -7,23 +7,18 @@
 #include <linux/time64.h>
 #include <linux/math64.h>
 
-extern u64 arch_sys_boot_offset;
-
 static int uptime_proc_show(struct seq_file *m, void *v)
 {
 	struct timespec64 uptime;
 	u64 total_uptime_nsec;
 	u64 idle_nsec;
-	u64 offset_secs;
 
+	/* boottime đã được fake ở core rồi, không cộng offset lần nữa */
 	ktime_get_boottime_ts64(&uptime);
 
-	if (arch_sys_boot_offset > 0) {
-		offset_secs = div_u64(arch_sys_boot_offset, 1000000000);
-		uptime.tv_sec += offset_secs;
-	}
-
 	total_uptime_nsec = (u64)uptime.tv_sec * NSEC_PER_SEC + uptime.tv_nsec;
+
+	/* Fake idle time: cố định 73% tổng uptime */
 	idle_nsec = div_u64(total_uptime_nsec * 73, 100);
 
 	seq_printf(m, "%lu.%02lu %lu.%02lu\n",
