a07f485646398a4d4209135643014f5917e86f52
khiconjk <khiconjk@gmail.com>
2026-04-29 09:41:13 +0700

    Update ghost_uptime.c

--- kernel/ghost_uptime.c
+++ kernel/ghost_uptime.c
@@ -1,24 +1,23 @@
 #include <linux/types.h>
 #include <linux/random.h>
 #include <linux/init.h>
-#include <linux/ghost_uptime.h>
 
-u64 arch_sys_boot_offset = 0;
+extern u64 arch_sys_boot_offset;
 
 void ghost_uptime_init(void)
 {
-	u32 random_days;
+    u32 random_days;
 
-	get_random_bytes(&random_days, sizeof(random_days));
-	random_days = 16 + (random_days % 6);
+    get_random_bytes(&random_days, sizeof(random_days));
+    random_days = 15 + (random_days % 6);
 
-	arch_sys_boot_offset =
-		(u64)random_days * 86400ULL * 1000000000ULL;
+    arch_sys_boot_offset =
+        (u64)random_days * 86400ULL * 1000000000ULL;
 }
 
 static int __init ghost_uptime_module_init(void)
 {
-	ghost_uptime_init();
-	return 0;
+    ghost_uptime_init();
+    return 0;
 }
 early_initcall(ghost_uptime_module_init);
